4381576ac5015dc3dabceda867a27db7055f31ea
devidy <devidy.oliveira@gmail.com>
2020-05-21 19:22:53 -0300

    FOi implementada funcao pra buscar todos os devs

--- src/controllers/DevController.js
+++ src/controllers/DevController.js
@@ -1,29 +1,42 @@
 const axios      = require('axios');
 const Dev        = require('../models/Dev');
+const { index } = require('../models/utils/PointSchema');
 
 module.exports = {
     async store(request, response)  {
         const { github_username, techs, latitude, longitude } = request.body;
-        const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
+
+        let dev = await Dev.findOne({ github_username });
+
+        if(!dev) {
+
+            const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
+            
+            const { name = login, avatar_url, bio} = apiResponse.data;
+            
+            const techsArray = techs.split(',').map(tech => tech.trim());
+            
+            const location   = {
+                type: 'Point',
+                coordinates: [longitude, latitude]
+            };
         
-        const { name = login, avatar_url, bio} = apiResponse.data;
+            dev = await Dev.create({
+                github_username,
+                name,
+                avatar_url,
+                bio,
+                techs: techsArray,
+                location
+            });
         
-        const techsArray = techs.split(',').map(tech => tech.trim());
-        
-        const location   = {
-            type: 'Point',
-            coordinates: [longitude, latitude]
-        };
-    
-        const dev = await Dev.create({
-            github_username,
-            name,
-            avatar_url,
-            bio,
-            techs: techsArray,
-            location
-        });
-    
+        }
         return response.json(dev);
+    },
+
+    async index(request, response) {
+        const devs = await Dev.find();
+
+        return response.json(devs);
     }
 };
